4e2c1f692c138f80586b1bd00fe76317d12ffc49
Marty Moradian <metaphorshearllc@gmail.com>
2017-10-15 12:21:28 -0500

    Fixed sort

--- hw5_inc/src/mergesort.rs
+++ hw5_inc/src/mergesort.rs
@@ -1,3 +1,5 @@
+//TODO: change ranges to be less confusing
+
 struct SortData<'a> {
     comps    : usize,
     v        : &'a mut Vec<i64>,
@@ -24,8 +26,9 @@ impl<'a> SortData<'a> {
         {
             //copy values from v into aux over the relevant range
             let mut tmp = self.aux.get_mut(lo..(hi+1)).unwrap(); //"inclusive range syntax is experimental"
-            let tm = {tmp.len()/2};
+            let tm = (tmp.len()-1)/2 + 1;
             let (tmpl, tmpr) = tmp.split_at_mut(tm); //necessary so the borrow system knows these slices don't overlap
+            //eprintln!("tmpl: {}, tmpr: {},left: {},right: {}", tmpl.len(), tmpr.len(), (mid+1-lo), (hi-mid));
             tmpl.copy_from_slice(self.v.get(lo..(mid+1)).unwrap()); 
             tmpr.copy_from_slice(self.v.get((mid+1)..(hi+1)).unwrap());
         } //now tmp is out of scope
